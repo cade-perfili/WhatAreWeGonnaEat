2f52bc02ea5a47210533e5909a1117ca706fadf1
cade-perfili <58614773+cade-perfili@users.noreply.github.com>
2022-10-13 18:59:44 -0400

    first commit and first code written

---
+++ WhatAreWeGonnaEat/WhatAreWeGonnaEat/Program.cs
@@ -0,0 +1,15 @@
+﻿public string Choice; 
+    Console.WriteLine("Hello, please make a selection");
+    Choice = Console.ReadLine();
+
+switch (Choice)
+{
+    case "1":
+        Console.WriteLine("You selected to add a receipie");
+        break;
+    case "2":
+        Console.WriteLine("You selected to run a plan");
+        break;
+}
+
+
