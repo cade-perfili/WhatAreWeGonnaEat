dc3af8a49ecb2aee8a72d9b5aed5c1f0af009fdd
cade-perfili <58614773+cade-perfili@users.noreply.github.com>
2022-10-19 18:33:19 -0400

    Added Recipie class skelton, name works. Adding ingredints to the class next

--- WhatAreWeGonnaEat/WhatAreWeGonnaEat/Program.cs
+++ WhatAreWeGonnaEat/WhatAreWeGonnaEat/Program.cs
@@ -1,15 +1,30 @@
-﻿public string Choice; 
+﻿
+using WhatAreWeGonnaEat;
+
+string Selection;
     Console.WriteLine("Hello, please make a selection");
-    Choice = Console.ReadLine();
+Console.WriteLine("Enter 1 to make your dinner plans!");
+Console.WriteLine("Enter 2 to enter in a new recipie!");
+Selection = Console.ReadLine();
 
-switch (Choice)
-{
-    case "1":
-        Console.WriteLine("You selected to add a receipie");
+    switch (Selection)
+    {
+        case "1":
+            Console.WriteLine("What's For Dinner?");
+            break;
+        case "2":
+        Console.WriteLine("What's the name of the recipie?");
+        Recipie myObj = new Recipie();
+        myObj.Name = Console.ReadLine();
+        Console.WriteLine(myObj.Name);
+            break;
+        case "3":
+            Console.WriteLine("Not eating something anymore? Let's get rid of it.");
         break;
-    case "2":
-        Console.WriteLine("You selected to run a plan");
+        case "4":
+        Console.WriteLine("Need to change a recipie?");
         break;
-}
-
-
+        case "5":
+        Console.WriteLine("I'm ready to eat, I'm done.");
+        break;
+    }
